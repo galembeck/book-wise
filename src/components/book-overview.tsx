d43be2da02af6ce0f7f2987fdfe485dbf8eeb698
Pedro Galembeck <galembeckpedro@gmail.com>
2025-04-10 17:04:13 -0300

    feat: create book-overview component

---
+++ src/components/book-overview.tsx
@@ -0,0 +1,9 @@
+export function BookOverview() {
+  return (
+    <section className="book-overview">
+      <div className="flex flex-1 flex-col gap-5">
+        <h1>Title</h1>
+      </div>
+    </section>
+  );
+}
