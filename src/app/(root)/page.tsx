838653b4bd0f69cbb13b499414358a4b3b31d859
Pedro Galembeck <galembeckpedro@gmail.com>
2025-04-10 17:02:27 -0300

    refactor: update root page.tsx

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { BookOverview } from "@/components/book-overview";
+import { BookList } from "@/components/book-list";
+
+export default function Home() {
+  return (
+    <>
+      <BookOverview />
+
+      <BookList />
+    </>
+  );
+}
